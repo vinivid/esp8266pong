1929512236ad6cf0a62c2ddd23ac58a6cc2f22ee
vini <frato.vini@gmail.com>
2024-07-01 13:42:23 -0300

    moving bar working

--- data/control.js
+++ data/control.js
@@ -1,8 +1,46 @@
-console.log(1);
+let posP1 = 50;
+let posP2 = 50;
 
-function moveUp(){
-    const rect = document.getElementById('rect1');
-    rect.style.top = `${rect.offsetTop - 5}px`;
+function moveBar(qtdMove, barToMove, barPos){
+    if(barPos >= 20 && barPos <= 75){
+        const rect = document.getElementById(barToMove);
+        let yinterval = setInterval(moveP1y, 5);
+        let count = 0;
+        function moveP1y(){
+            if(count == 10){
+                count = 0;
+                clearInterval(yinterval);
+            }else{
+                barPos += (0.2)*qtdMove;
+                count += 1;
+                rect.style.top = barPos + 'vh';
+            }
+        }
+    }
 }
 
-console.log(2);
+setInterval(function executeMoveP1(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            moveBar(xhttp.responseText, 'rect1', posP1);
+            console.log(xhttp.responseText);
+        }
+    };
+    xhttp.open("GET", "/p1", true);
+    xhttp.send();
+}, 100);
+
+/* function executeMoveP2(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            moveBar(xhttp.responseText, 'rect2', posP2);
+            console.log(xhttp.responseText);
+        }
+    };
+    xhttp.open("GET", "/p2", true);
+    xhttp.send();
+}
+
+setInterval(executeMoveP2(), 100); */
--- esp8266pong.ino
+++ esp8266pong.ino
@@ -1,54 +1,77 @@
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <FS.h>
-#include "LittleFS.h"
+#include <LittleFS.h>
 
-// assigning the pins to be used
-const int buttonYUp = 5;
-const int buttonYDown = 4;
+//Coloca o nome do WiFi e a senha a ser usada
+const char* ssid = "esp8266pong";
+const char* passWord = "123456789";
 
-//size of the player pong racket
-int p1bary = 0;
-int p1size = 5;
-
-//position of the ball in the plane
-int ballx = 0;
-int bally = 0;
-
-//the direction the ball is going to go depending on the state of the game
-int currxdir = -1;
-
-//limits of the plane
-int yaxis = 50;
-int xaxis = 50;
-
-const char* ssid = "PongNoESP";
-const char* password = "1234";
-
+//inicializa um instancia servidor no port 80
 AsyncWebServer server(80);
 
-void setupPin(void){
-  pinMode(buttonYUp, INPUT);
-  pinMode(buttonTDown, INPUT);
-}
+int pin1 = 5;
+int pin2 = 4;
+int pin5 = 14;
+int pin6 = 12;
 
-void setup() {
+void setup(){
   Serial.begin(9600);
 
+  /*Inicia o WiFi no modo de soft acess point
+  */
+  WiFi.softAP(ssid, passWord);
+  IPAddress IP = WiFi.softAPIP();
+
+  Serial.println(IP);
+  Serial.println(WiFi.localIP());
+
+  pinMode(pin5, OUTPUT);
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, INPUT);
+  pinMode(pin6, INPUT);
+
+  //Inicia o server
+  server.begin();
+
+  //inicia o sistema de file do ESP
   if(!LittleFS.begin()){
-    Serial.println("Error in LittleFS");
-    return;
+    Serial.println("Erro ao abrir littleFS"); return;
   }
 
-  
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/index.html");
+    Serial.println("Sucess html");
+  });
 
-  setupPin();
+  server.on("/look.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/look.css");
+    Serial.println("Sucess css");
+  });
+
+  server.on("/control.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/control.js");
+    Serial.println("Sucess js");
+  });
+
+  server.on("/p1", HTTP_GET, [](AsyncWebServerRequest *request){
+    if(digitalRead(pin2) == HIGH){
+      request->send_P(200, "text/plain", "1");
+      Serial.println("kinda");
+    }else if(digitalRead(pin6) == HIGH){
+      request->send_P(200, "text/plain", "-1");
+      Serial.print("neg");
+    }else request->send_P(204, "text/plain", "0");
+  });
 }
 
-void loop() {
-  int leftButton = digitalRead(buttonYUp);
-  int rightButton = digitalRead(buttonYDown);
-
+void loop(){
+  digitalWrite(pin1, HIGH);
+  delay(300);
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin5, HIGH);
+  delay(300);
+  digitalWrite(pin5, LOW);
 }
+
