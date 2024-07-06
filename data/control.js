84534edfbf7a0283e5d7b1bd36d5408d1b22615c
vinivid <frato.vini@gmail.com>
2024-07-06 16:26:52 -0300

    remaking the pong part

--- data/control.js
+++ data/control.js
@@ -1,5 +1,10 @@
-let posP1 = 50;
-let posP2 = 50;
+//All units in px
+//there are problems on vertical mobile
+
+let posP1 = 440;
+let TposP1 = 440;
+let posP2 = 440;
+let TposP2 = 440;
 let ballPosX = 50;
 let ballPosY = 50;
 let ballXdir = -1;
@@ -8,7 +13,6 @@ let ballYvel = 0.02;
 let pointsP1 = 0;
 let pointsP2 = 0;
 
-//randomiza a direção inicial da bola
 function randomizeStart(){
     ballPosX = 50;
     ballPosY = 50;
@@ -30,14 +34,92 @@ function randomizeStart(){
 
 randomizeStart();
 
-/*A função moveP1() move o bastão do jogador verticalmente com uma pequena
-animação, tabém inibindo que o bastão va além dos limites.
+/*Pos top = 110px Pos bot = 800px vertical limits
+< 250px || > 1550px horizontal limits
 
-Argumento:
-qtdMove: é o valor passado pelo HTTP_GET que indica para qual direção
-o bastão esta sendo movido.
+rect heigh = 100px
 */
-function moveP1(qtdMove){
+
+//Moves the player 20px vertically
+function moveP1(yMoveDir){
+    const rect = document.getElementById('rect1');
+
+    //to avoid the bar entering or going beyond the limit
+    if(posP1 <= 130){
+        if(yMoveDir == 1){
+            TposP1 += 20 * (yMoveDir);
+            let intervalAnim = setInterval(moveFunc, 2);
+            let count = 0;
+    
+            function moveFunc(){
+                if(count == 4) clearInterval(intervalAnim);
+                posP1 += 5 * (yMoveDir);
+                count += 1;
+                rect.style.top = posP1;
+            }
+
+            return;
+        }
+
+        let relativePos = posP1 - 110;
+
+
+        TposP1 += relativePos * (yMoveDir);
+        let intervalAnim = setInterval(moveFunc, 2);
+        let count = 0;
+
+        function moveFunc(){
+            if(count == 4) clearInterval(intervalAnim);
+            posP1 += (relativePos/4) * (yMoveDir);
+            count += 1;
+            rect.style.top = posP1;
+        }
+
+    }else if(posP1 >= 680){
+        if(yMoveDir == -1){
+            TposP1 += 20 * (yMoveDir);
+            let intervalAnim = setInterval(moveFunc, 2);
+            let count = 0;
+    
+            function moveFunc(){
+                if(count == 4) clearInterval(intervalAnim);
+                posP1 += 5 * (yMoveDir);
+                count += 1;
+                rect.style.top = posP1;
+            }
+
+            return;
+        }
+
+        let relativePos = 700 - posP1 
+
+
+        TposP1 += relativePos * (yMoveDir);
+        let intervalAnim = setInterval(moveFunc, 2);
+        let count = 0;
+
+        function moveFunc(){
+            if(count == 4) clearInterval(intervalAnim);
+            posP1 += (relativePos/4) * (yMoveDir);
+            count += 1;
+            rect.style.top = posP1;
+        }
+
+    }else{
+        TposP1 += 20 * (yMoveDir);
+        let intervalAnim = setInterval(moveFunc, 2);
+        let count = 0;
+
+        function moveFunc(){
+            if(count == 4) clearInterval(intervalAnim);
+            posP1 += 5 * (yMoveDir);
+            count += 1;
+            rect.style.top = posP1;
+        }
+    }
+}
+
+function fmoveP1(qtdMove){
     const rect = document.getElementById('rect1');
     if(posP1 > 15 && posP1 < 80){
         let yinterval = setInterval(moveP1y, 2);
@@ -91,126 +173,10 @@ function moveP2(qtdMove){
     }
 }
 
-/*A função moveBall() move a bola e também visualiza ela no HTML com
-uma animação, assim como checa se um jogador ganhou um ponto.
 
-A velocidade do deslocamento horizontal é constante e o vertical
-depende das reflexões.
-
-Quando a bola chega a um dos limites verticais apenas se inverte
-o a direção do deslocamento Y.
-
-Quando a bola vai bater no bastão de um dos jogadores o programa
-calcula a reflexão da seguinte forma:
-
-Para o eixo x ele apenas inverte sua direção.
-
-Para o y magine que um bastão começa na coordenada (0,0) e seu tamanho
-é h tal que (0, h) é seu fim. A posição do meio sera dada por (0, h/2)
-e ele sera o parametro para a reflexão.
-
-Se a bola atingir o bastão na parte de cima (de (0,0) até (0,h/2)) ele
-refletira para cima, com a velocidade vertical indo de 0 até 1, sendo
-que se ela bater exatamente no centro a velocidade sera 0 e exatamente
-na ponta 1.
-A parte de baixo segue a mesma lógica porém refletindo para baixo.
-
-É notavel que tudo é feito com base na parte de cima da bola (+2.655)
-Existe um bug que de alguma forma quando a bola atinge um dos extremos
-cantos da pilastra ela vai na direção contraria que deveria.
-
-Os pontos são calculados quando a bola chega um pouco além de um dos
-bastões, que aumenta um no placar de um dos jogadores e também renicia
-a posição da bola.
-*/
-let ballMovement = setInterval(moveBall, 100);
-
-function moveBall(){
-    if(ballPosY > 16 && ballPosY < 90){
-        const ball = document.getElementById('ball');
-        let mainInterval = setInterval(moveBallXY, 5)
-        let count = 0;
-        function moveBallXY(){
-            if(count == 15){
-                count = 0;
-                clearInterval(mainInterval);
-            }else{
-                ballPosX += -(0.15) * ballXdir;
-                ballPosY += ballYvel * ballYdir;
-
-                //inverte direção do y quando a bola chega em um dos limites
-                if(ballPosY <= 16 || ballPosY >= 90) ballYdir = ballYdir*(-1);
-                
-                //resolve se a bola foi refletida ou não por um jogador
-                if(ballPosX <= 15.7 && ballPosX >= 15.35625){
-                    if((posP1) <= (ballPosY + 2.655) && (posP1 + 6.25) >= ballPosY ){
-                        ballXdir = ballXdir * (-1);
-                        ballPosX += 0.30;
-                        ballYvel = ((posP1+6.25) - (ballPosY+2.655))/(6.25);
-                    }else if((posP1+6.25) < (ballPosY + 2.655) && (posP1+12.5)>= ballPosY){
-                        ballXdir = ballXdir * (-1);
-                        ballPosX += 0.30;
-                        ballYvel = ((ballPosY+2.655) - (posP1+6.25))/(6.25);
-                    }                                                          
-                }
-                if(ballPosX >= (84.15625) && ballPosX <= 84.6){
-                    if((posP2) <= (ballPosY + 2.655) && (posP2 + 6.25) >= ballPosY ){
-                        ballXdir = ballXdir * (-1);
-                        ballPosX -= 0.30;
-                        ballYvel = ((posP2+6.25) - (ballPosY+2.655))/(6.25);
-                    }else if((posP2+6.25) < ballPosY && (posP2+12.5)>= ballPosY){
-                        ballXdir = ballXdir * (-1);
-                        ballPosX -= 0.30;
-                        ballYvel = ((ballPosY+2.655) - (posP2+6.25))/(6.25);
-                    }   
-                }
-
-                //contabiliza os pontos
-                if(ballPosX <= 12){
-                    pointsP2 += 1;
-                    const htp2 = document.getElementById('p2point');
-                    htp2.innerHTML = pointsP2;
-                    randomizeStart();
-                }
-                if(ballPosX >= 87){
-                    pointsP1 += 1;
-                    const htp1 = document.getElementById('p1point');
-                    htp1.innerHTML = pointsP1;
-                    randomizeStart();
-                }
-
-                count += 1;
-                ball.style.top = ballPosY + 'vh';
-                ball.style.left = ballPosX + 'vw';
-            }
-        }
-    }else if(ballPosY <= 16){
-        ballPosY = 16.01;
-    }else{
-        ballPosY = 89.09
-    }    
-}
-
-
-//São as funções que constantemente checam se algum dos botões esta pressionado
-setInterval(() => {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            moveP1(xhttp.responseText);
-        }
-    };
-    xhttp.open("GET", "/p1", true);
-    xhttp.send();
-}, 50);
-
-setInterval(() => {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            moveP2(xhttp.responseText);
-        }
-    };
-    xhttp.open("GET", "/p2", true);
-    xhttp.send();
-}, 50);
+document.addEventListener('keydown', (event)=>{
+    if(event.key == "w") moveP1('-1');
+    if(event.key == "s") moveP1('1');
+    if(event.key == "i") moveP2('-1');
+    if(event.key == "k") moveP2('1');
+})
